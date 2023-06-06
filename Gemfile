22e9511bf86b918510ef940224ad97e50ab4c883
Simon Mitchell <simon.mitchell@dvusd.org>
2023-06-06 11:04:53 -0700

    Initial change, pruning files

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source "https://rubygems.org"
-gem "jekyll", "~> 4.3.2"
-group :jekyll_plugins do
-  gem "jekyll-mentions"
-  gem "jekyll-deploy"
-  gem "jekyll-webp"
-end
-
-
--- send-mail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /root/firewx
-
-source $(pwd)/config
-
-logger "Emailing morning fire weather briefing."
-
-echo -e "The $title for $(date) has been updated.\n\nRemember to view it at \
-$url." | mail -s "$title $(date)" -r "$from" "$email"
